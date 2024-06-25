bdf7a3ee418b7c2cbe9873c06671cc9ca0a25a91
RagsterTh <139650575+RagsterTh@users.noreply.github.com>
2024-06-25 16:39:11 -0300

    att

--- GolfDungeon/Assets/Scripts/Hole.cs
+++ GolfDungeon/Assets/Scripts/Hole.cs
@@ -20,8 +20,19 @@ public class Hole : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             Rigidbody2D rigidbody = collision.GetComponent<Rigidbody2D>();
-            if(rigidbody.velocity.magnitude < 3)
-                collision.gameObject.SetActive(false);
+            if (rigidbody.velocity.magnitude < 3)
+                StartCoroutine(BallEntering(collision.transform));
         }
+    }
+    IEnumerator BallEntering(Transform playerScale)
+    {
+        Rigidbody2D ballBody = playerScale.GetComponent<Rigidbody2D>();
+        while (playerScale.localScale.x > 0) 
+        {
+            yield return new WaitForSeconds(0.2f);
+            ballBody.velocity -= new Vector2(1.5f, 1.5f);
+            playerScale.localScale -= new Vector3(0.1f, 0.1f);
+        }
+        playerScale.gameObject.SetActive(false);
     }
 }
