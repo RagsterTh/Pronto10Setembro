9286a61d2e77ae8f86bf86931b27df28a08769f2
RagsterTh <139650575+RagsterTh@users.noreply.github.com>
2024-07-03 14:39:52 -0300

    att

--- GolfDungeon/Assets/Scripts/Hole.cs
+++ GolfDungeon/Assets/Scripts/Hole.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Hole : MonoBehaviour
@@ -24,13 +25,24 @@ public class Hole : MonoBehaviour
                 StartCoroutine(BallEntering(collision.transform));
         }
     }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Rigidbody2D rigidbody = collision.GetComponent<Rigidbody2D>();
+            print("rolou");
+            if (rigidbody.velocity.magnitude < 1)
+                rigidbody.velocity = Vector2.zero;
+        }
+    }
     IEnumerator BallEntering(Transform playerScale)
     {
         Rigidbody2D ballBody = playerScale.GetComponent<Rigidbody2D>();
+        ballBody.velocity = Vector3.Lerp(ballBody.velocity, Vector2.zero, 0.7f);
         while (playerScale.localScale.x > 0) 
         {
             yield return new WaitForSeconds(0.2f);
-            ballBody.velocity -= new Vector2(1.5f, 1.5f);
+
             playerScale.localScale -= new Vector3(0.1f, 0.1f);
         }
         playerScale.gameObject.SetActive(false);
